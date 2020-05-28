57690a4f8c939f15bd5cc35ccab51d76b0274f28
ChenYong <chenyong@jimeng.io>
2020-05-28 14:39:22 +0800

    upgrade deps

--- example/main.tsx
+++ example/main.tsx
@@ -11,9 +11,9 @@ import Container from "./pages/container";
 import { GenRouterTypeMain } from "controller/generated-router";
 
 const renderApp = () => {
-  let routerTree = parseRoutePath(window.location.hash.slice(1), routerRules) as GenRouterTypeMain;
+  let routerTree = parseRoutePath(window.location.hash.slice(1), routerRules);
 
-  ReactDOM.render(<Container router={routerTree} />, document.querySelector(".app"));
+  ReactDOM.render(<Container router={routerTree as any} />, document.querySelector(".app"));
 };
 
 window.onload = renderApp;
